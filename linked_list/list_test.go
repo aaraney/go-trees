e3c08fa2b1689cfb6782950f7dae0b0c1616781d
Austin Raney <aaraney@protonmail.com>
2022-08-24 14:52:37 -0400

    update list tests

--- linked_list/list_test.go
+++ linked_list/list_test.go
@@ -46,3 +46,43 @@ func TestIter(t *testing.T) {
 	}
 
 }
+
+func TestDeleteListItem(t *testing.T) {
+	root := NewList(1, 2)
+	one, _ := root.Get(1)
+
+	err := root.Delete(1)
+	if err != nil {
+		t.Errorf("should not have received err: %q", err)
+	}
+
+	if root.Root().Value != 2 {
+		t.Error("root should have value of 2")
+	}
+
+	if one.Next != nil {
+		t.Error("deleted item should have nil Next pointer", err)
+		t.Errorf("%s", one)
+	}
+
+	root = NewList(1, 2, 3)
+	root.Delete(2)
+	root.Delete(1)
+
+	if root.Root().Value != 3 {
+		t.Error("root should have value of 3")
+	}
+
+	iter := root.Iter()
+	target_n := 1
+	n := 0
+
+	// count number of items in list
+	for item := iter.Next(); item != nil; item = iter.Next() {
+		n++
+	}
+
+	if target_n != n {
+		t.Errorf("expected: %d got: %d. %d != %d. too many items in list.", target_n, n, target_n, n)
+	}
+}
